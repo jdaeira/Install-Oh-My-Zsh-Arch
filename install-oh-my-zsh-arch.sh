1ee209d6e20eb14b33312f2ccaca0c7ebb05fe79
John da Eira <jdaeira@gmail.com>
2022-04-08 15:51:01 -0700

    Create install-oh-my-zsh-arch.sh

---
+++ install-oh-my-zsh-arch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -S zsh
+
+## Install Pi Theme
+wget -O $ZSH_CUSTOM/themes/pi.zsh-theme https://raw.githubusercontent.com/tobyjamesthomas/pi/master/pi.zsh-theme
+
+## nano ~/.zshrc
+## ZSH_THEME="pi"
+## source ~/.zshrc
+
+## Install Auto Suggestions Plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+## Install Syntax Highlighting Plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+## nano ~/.zshrc
+## plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+## source ~/.zshrc
